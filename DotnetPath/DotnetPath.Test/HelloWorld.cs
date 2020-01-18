9d74afaf736f8ef68004c5b71276fedf047db05f
Erick Concepcion <erickconcepcion@live.com>
2020-01-18 14:05:38 -0400

    failing test to ternary and nullcheck

--- DotnetPath/DotnetPath.Test/HelloWorld.cs
+++ DotnetPath/DotnetPath.Test/HelloWorld.cs
@@ -11,5 +11,33 @@ namespace DotnetPath.Test
             var a = 2 + 2;
             Assert.Equal(a, 4);
         }
+        [Fact]
+        public void TestTernary()
+        {
+            int a = 0;
+            int b = 0;
+            var cantBeZero = true;
+            if (cantBeZero)
+            {
+                b += 1;
+            }
+            // Use ternary to asing 1 on a variable.
+
+            Assert.Equal(a, b);
+        }
+        [Fact]
+        public void TestNullCheck()
+        {
+            string a = null;
+            string b = null;
+            if (string.IsNullOrEmpty(b))
+            {
+                b = "Hello";
+            }
+            //use the ?? null check operator to asing other value to a Variable
+            Assert.NotNull(b);
+            Assert.NotNull(a);
+
+        }
     }
 }
